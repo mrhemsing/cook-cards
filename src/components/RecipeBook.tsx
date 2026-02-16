0b3cc3cd15b10865347e99e8be19f40e00f7b315
Matt Hemsing <46225546+mrhemsing@users.noreply.github.com>
2026-02-15 23:37:53 -0800

    Add desktop top and bottom page margins

--- src/components/RecipeBook.tsx
+++ src/components/RecipeBook.tsx
@@ -275,7 +275,7 @@ export default function RecipeBook() {
       </header>
 
       {/* Main Content */}
-      <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
+      <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8 lg:mt-[10px] lg:mb-[10px]">
         {/* Action Buttons */}
         <div className="flex flex-col sm:flex-row gap-4 mb-8">
           <button
